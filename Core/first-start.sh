4f66252eee2aa7fec8acc5417d184473c59793ac
Mohammad <mohammad@truelayer.com>
2019-02-19 23:04:31 +0000

    Assume unchange csproj

--- Core/first-start.sh
+++ Core/first-start.sh
@@ -5,6 +5,7 @@
 git clone git@github.com:mustakimali/cant-run-linqpad.git
 cd cant-run-linqpad
 git update-index --assume-unchanged Program.cs
+git update-index --assume-unchanged cant-run-linqpad.csproj
 code .
 
 # start.sh
